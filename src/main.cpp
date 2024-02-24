7e9065d8f98209bd07ded92ddaf37cd6cb3730ad
andrewens <andrewens4@gmail.com>
2024-02-24 13:01:52 -0600

    Compile a c++ hello world

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+  
+// Driver Code 
+int main() 
+{ 
+    std::cout << "Hello world!\n";
+    int x;
+    std::cin >> x;
+    std::cout << "Your number is " << x << "\n";
+    return 0; 
+}

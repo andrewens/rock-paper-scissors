aea73517f11d507a50668812e7bb197cd4eea644
andrewens <andrewens4@gmail.com>
2024-02-24 13:17:26 -0600

    rock paper scissors input loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 
 // Driver Code 
 int main() 
 { 
+	// seed the random number generator
 	srand(time(NULL));
 
-	std::cout << "Hello world!\n";
-
-	int rand_num = std::rand() % 10 + 1;
-	std::cout << "Random number: " << rand_num << "\n";
+	// gsgh
+	std::cout << "Rock, Paper, or Scissors?: ";
+	
+	int rps_value = -1;
+	while (rps_value < 0) {
+		std::string ans;
+		std::cin >> ans;
+		
+		if (ans == "rock") {
+			rps_value = 0;
+		}
+		else {
+			std::cout << ans << " is not a valid response. Try again: ";
+		}
+	}
 
 	return 0; 
 }
